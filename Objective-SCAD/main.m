899e81a9d03aea8ff995279aa1dfda9a9e599e81
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-01-19 01:25:42 -0600

    Initial Commit

---
+++ Objective-SCAD/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Objective-SCAD
+//
+//  Created by Jeffrey Camealy on 1/19/14.
+//  Copyright (c) 2014 bearMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
