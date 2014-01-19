deffa00f87ab31a14c91ee47b7942f8409cbc73d
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-01-19 14:26:38 -0600

    remove SpiralSphere, add headers include file

---
+++ Objective-SCAD/Objective-SCAD/ObjectiveSCADHeaders.h
@@ -0,0 +1,14 @@
+//
+//  ObjectiveSCADHeaders.h
+//  StrollerHook
+//
+//  Created by Jeffrey Camealy on 1/19/14.
+//  Copyright (c) 2014 bearMountain. All rights reserved.
+//
+
+#ifndef StrollerHook_ObjectiveSCADHeaders_h
+#define StrollerHook_ObjectiveSCADHeaders_h
+
+#import "OSCube.h"
+
+#endif
