7521649d64afa4cfe3e2835589d76921d36cf023
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-01-20 14:11:36 -0600

    add back ossphere

---
+++ Objective-SCAD/Objective-SCAD/OSSphere.h
@@ -0,0 +1,17 @@
+//
+//  OSSphere.h
+//  StrollerHook
+//
+//  Created by Jeffrey Camealy on 1/20/14.
+//  Copyright (c) 2014 bearMountain. All rights reserved.
+//
+
+#import "OSObject.h"
+
+@interface OSSphere : OSObject
+
+- (id)initWithRadius:(float)radius;
+
+@property float radius;
+
+@end
---
+++ Objective-SCAD/Objective-SCAD/OSSphere.m
@@ -0,0 +1,29 @@
+//
+//  OSSphere.m
+//  StrollerHook
+//
+//  Created by Jeffrey Camealy on 1/20/14.
+//  Copyright (c) 2014 bearMountain. All rights reserved.
+//
+
+#import "OSSphere.h"
+
+@implementation OSSphere
+
+#pragma mark - Init Method
+
+- (id)initWithRadius:(float)radius {
+    if (self = [super init]) {
+        _radius = radius;
+    }
+    return self;
+}
+
+
+#pragma mark - Override
+
+- (NSString *)scad {
+    return [NSString stringWithFormat:@"sphere(%g);", self.radius];
+}
+
+@end
